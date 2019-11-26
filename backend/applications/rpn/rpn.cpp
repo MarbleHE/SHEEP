1fefd2ee551ab161dc38c6263f9b93b755662d26
Mario Stöckli <stmario@ethz.ch>
2019-11-26 22:33:42 +0100

    Added sekeltons for rpn composition and calculation.

--- backend/applications/rpn/rpn.cpp
+++ backend/applications/rpn/rpn.cpp
@@ -1,11 +1,15 @@
 #include <string>
 #include <istream>
 #include <sstream>
+#include <stack>
 #include "rpn.hpp"
 #include "token.hpp"
+#include "simple-circuits.hpp"
+
 
 using namespace std;
 
+//Transform to tokens cointaining ints or ops. Whitespace separator.
 Rpn::Rpn(string calculation){
     vector<Token> tokens;
     istringstream iss(calculation);
@@ -13,8 +17,22 @@ Rpn::Rpn(string calculation){
     while (getline(iss, s, ' ')){
         tokens.push_back(Token(s));
     }
+    calc = tokens;
 }
 
+//composes a circuit with the desired library and calculations and runs it.
 void Rpn::calcWith(int library){
+    Circuit c = composeCircuit(library);
+    //then run.
+}
 
+//composes a circuit given a library based on the calculation stored in the object.
+Circuit Rpn::composeCircuit(int library){
+    stack<Token> s;
+    for (auto i = calc.begin(); i != calc.end(); ++i){
+        Op cop = i->op;
+
+    }
+    
+    //return ;
 }
