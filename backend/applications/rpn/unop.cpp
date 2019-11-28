92995b02b8c87761863844d925b79297654d952d
Mario Stöckli <stmario@ethz.ch>
2019-11-28 14:33:57 +0100

    Added unop handleOp definition.

--- backend/applications/rpn/unop.cpp
+++ backend/applications/rpn/unop.cpp
@@ -2,6 +2,8 @@
 #include <stack>
 #include "unop.hpp"
 #include "circuit.hpp"
+#include "simple-circuits.hpp"
+#include "circuit-util.hpp"
 
 using namespace std;
 
@@ -9,4 +11,42 @@ UnOp::UnOp(unoptype unopt){
     utp = unopt;
 }
 
-void UnOp::handleOp(vector<int> *ptvec, stack<Circuit> *s){}
+void UnOp::handleOp(vector<int> *ptvec, stack<Circuit> *s){
+    if (s->size() < 1)
+    {
+        throw runtime_error("Stack has not enough input for unary gate.");
+    }
+    
+    Circuit uc;
+    switch (utp)
+    {
+    case unoptype::Negate:
+    {
+        uc = single_unary_gate_circuit(Gate::Negate);
+        Circuit l;
+        l = s->top();
+        s->pop();
+        Circuit sc = seq(uc, l);
+        s->push(sc);
+        break;
+    }
+    case unoptype::Square:
+    {
+        uc = single_binary_gate_circuit(Gate::Multiply);
+        Circuit l;
+        l = s->top();
+        s->pop();
+        Circuit sc = seq(uc, par(l, l));
+        s->push(sc);
+        break;
+    }
+    /*case unoptype::SquareRoot:
+        throw GateNotImplementedError
+        break;
+    case unoptype::Invert:
+        throw GateNotImplementedError
+        */
+    default:
+        break;
+    }
+}
