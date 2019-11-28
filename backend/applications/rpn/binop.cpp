f99a3f0f52ba93b95c00fac8b637ddaf5e2a85a6
Mario Stöckli <stmario@ethz.ch>
2019-11-28 14:18:43 +0100

    Added binop handleOp function.

--- backend/applications/rpn/binop.cpp
+++ backend/applications/rpn/binop.cpp
@@ -3,6 +3,7 @@
 #include "binop.hpp"
 #include "circuit.hpp"
 #include "simple-circuits.hpp"
+#include "circuit-util.hpp"
 
 using namespace std;
 
@@ -12,15 +13,34 @@ BinOp::BinOp (binoptype binopt){
 
 
 void BinOp::handleOp(vector<int> *ptvec, stack<Circuit> *s){
+    if (s->size() < 2)
+    {
+        throw runtime_error("Stack has not enough input for binary gate.");
+    }
+    
     Circuit bc;
     switch (btp)
     {
     case binoptype::Add:
         bc = single_binary_gate_circuit(Gate::Add);
-
         break;
-    
+    case binoptype::Subtract:
+        bc = single_binary_gate_circuit(Gate::Subtract);
+        break;
+    case binoptype::Multiply:
+        bc = single_binary_gate_circuit(Gate::Multiply);
+        break;
+    /*case binoptype::Divide:
+        throw GateNotImplementedError*/
     default:
         break;
     }
+    Circuit l;
+    Circuit r;
+    l = s->top();
+    s->pop();
+    r = s->top();
+    s->pop();
+    Circuit combc = seq(bc, par(l, r));
+    s->push(combc);
 }
