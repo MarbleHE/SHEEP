87432d003bf5ed353fcc669224ca9c8775e6b105
Mario Stöckli <stmario@ethz.ch>
2019-11-28 15:19:15 +0100

    added minBits definition and description.

--- backend/applications/rpn/rpn.cpp
+++ backend/applications/rpn/rpn.cpp
@@ -2,10 +2,10 @@
 #include <istream>
 #include <sstream>
 #include <stack>
+#include <cmath>
 #include "rpn.hpp"
 #include "token.hpp"
 #include "op.hpp"
-//#include "simple-circuits.hpp"
 
 enum libs {Plaintext = 0}; //TODO add all
 
@@ -22,9 +22,11 @@ Rpn::Rpn(string calculation){
     calc = tokens;
 }
 
+
 //composes a circuit with the desired library and calculations and runs it.
 void Rpn::calcWith(int library){
     composeCircuit(library);
+    inttype  minBits();
     switch (library)
     {
     case Plaintext:
@@ -40,12 +42,10 @@ void Rpn::calcWith(int library){
 //composes a circuit given a library based on the calculation stored in the object.
 void Rpn::composeCircuit(int library){
     stack<Circuit> *s = new stack<Circuit>;
-    int counter = 0;
     for (auto i = calc.begin(); i != calc.end(); ++i){
         Op *cop = i->op;
         cop->handleOp(&ptvec, s);
         //compose circuit, make all plaintext numbers ciphertexts and put into private array for eval. Or maybe I nee to put them into a mapping int,Wire...?
-        counter++;
     }
     // stack should now only contain the final circuit.
     if (s->size() > 1)
@@ -55,4 +55,33 @@ void Rpn::composeCircuit(int library){
     c = s->top();
     s->pop();
     delete s;
+}
+
+/*minBits determines the minimal required number of bits based on the user input
+It is a very easy implementation, assuming no overflow happens in the circuit.
+The only thing checked is whether an encoding of each plaintext is possible.
+*/
+inttype Rpn::minBits(){
+    int cmax = 0;
+    for (auto i = ptvec.begin(); i != ptvec.end(); ++i){
+        cmax = max(cmax,abs(*i));
+    }
+    int n_bits = (int) ceil(log(cmax));
+    if (n_bits < 8)
+    {
+        return inttype::INT_8;
+    }
+    if (n_bits < 16)
+    {
+        return inttype::INT_16;
+    }
+    if (n_bits < 32)
+    {
+        return inttype::INT_32;
+    }
+    if (n_bits < 64)
+    {
+        return inttype::INT_64;
+    }
+    throw invalid_argument("Integer too large for 64 bits");
 }
--- backend/applications/rpn/rpn.hpp
+++ backend/applications/rpn/rpn.hpp
@@ -7,6 +7,9 @@
 using namespace std;
 using Calculation = vector <Token>;
 
+// This is to determine the type of the context template
+enum class inttype {INT_8, INT_16, INT_32, INT_64};
+
 /*This is a representation of the calculator. It is initialized by the String input which gets transformed
     at construction to to Tokens containing the operations. The circuit composition happens lazily when a
     calculation with a specified library is requested.
@@ -21,4 +24,5 @@ class Rpn{
         void composeCircuit(int library);
         Circuit c;
         vector<int> ptvec;
+        inttype minBits();
 };
