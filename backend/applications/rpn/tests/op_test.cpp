dfc1703926d50ca3bcea2dbd6b64c68745ff283c
Mario Stöckli <stmario@ethz.ch>
2019-12-22 22:04:14 +0100

    Adding google test submodule and cmake files.

--- backend/applications/rpn/tests/op_test.cpp
+++ backend/applications/rpn/tests/op_test.cpp
@@ -1,4 +1,12 @@
 //
 // Created by mario on 22.12.19.
 //
+#include "gtest/gtest.h"
 
+#include "op.hpp"
+#include "intop.hpp"
+
+TEST(Basic, OneIsOne){
+    auto intop = IntOp(1);
+    ASSERT_EQ(intop.i, 1);
+}
--- backend/applications/rpn/tests/rpn_test.cpp
+++ backend/applications/rpn/tests/rpn_test.cpp
@@ -1,4 +1,7 @@
 //
 // Created by mario on 22.12.19.
 //
+#include "gtest/gtest.h"
+
+#include "rpn.hpp"
 
