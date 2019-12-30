b2bd573901a7c5d893b2b858e6f26ab9f2a6bc01
Mario Stöckli <stmario@ethz.ch>
2019-12-30 18:31:22 +0100

    Adding test fixtures and parameterized test stubs.

--- backend/applications/rpn/tests/tests.cpp
+++ backend/applications/rpn/tests/tests.cpp
@@ -10,6 +10,12 @@
 
 #include "gtest/gtest.h"
 
+using namespace std;
+
+/*
+ * Basic tests for CMake build.
+ */
+
 TEST(BasicIntop__Test, One)
 {
     auto i = IntOp(1);
@@ -22,6 +28,57 @@ TEST(BasicUnop__Test, Negate){
     ASSERT_EQ(unoptype::Negate, u->utp);
 }
 
+/* TODO
+ * Test all valid token generations.
+ */
+class TokenTests: ::testing::Test{
+protected:
+
+    void SetUp() override{Test::SetUp();};
+    void TearDown() override{Test::TearDown();};
+};
+
+/* TODO
+ * Test handleOp
+ * Maybe it is smarter to use instantiate some Rpn objects, instead of comparing vectors and stacks...
+ */
+class RpnTest: ::testing::Test{
+protected:
+    Rpn rpn;
+    void SetUp() override{Test::SetUp();};
+    void TearDown() override{Test::TearDown();};
+};
+struct handleState {
+    Op* operation;
+    vector<int> initial_ptvec;
+    vector<int> expected_ptvec;
+    stack<Circuit> initial_stack;
+    stack<Circuit> expected_stack;
+};
+class HandleOpTest: RpnTest, ::testing::WithParamInterface<handleState>{
+protected:
+    Op* operation;
+    vector<int> ptvec;
+    stack<Circuit> s;
+    void SetUp() override
+    {
+        operation = GetParam().operation;
+        ptvec = GetParam().initial_ptvec;
+        s = GetParam().initial_stack;
+    };
+};
+
+TEST_P(HandleOpTest, Int){
+    operation->handleOp(ptvec, s);
+    EXPECT_EQ(ptvec, GetParam().expected_ptvec);
+    EXPECT_EQ(s, GetParam().expected_stack);
+}
+
+INSTANTIATE_TEST_CASE_P(Default, HandleOpTest,
+        testing::Values(
+                handleState{}
+                )
+        );
 
 
 int main(int argc, char **argv) {
