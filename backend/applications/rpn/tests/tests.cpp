bcc728fc7f123604ff0292eebc587b8cbb2f156e
Mario Stöckli <stmario@ethz.ch>
2020-01-04 14:47:38 +0100

    Added tests. In the last commit the CMakeLists file was missing, so here it is now.

--- backend/applications/rpn/tests/tests.cpp
+++ backend/applications/rpn/tests/tests.cpp
@@ -1,12 +1,17 @@
 //
 // Created by mario on 30.12.19.
 //
+#include <deque>
+#include <stack>
+#include <vector>
+
 #include "op.hpp"
 #include "binop.hpp"
 #include "unop.hpp"
 #include "intop.hpp"
 #include "rpn.hpp"
 #include "token.hpp"
+#include "simple-circuits.hpp"
 
 #include "gtest/gtest.h"
 
@@ -31,55 +36,83 @@ TEST(BasicUnop__Test, Negate){
 /* TODO
  * Test all valid token generations.
  */
-class TokenTests: ::testing::Test{
+class TokenTests:public ::testing::Test{
 protected:
-
     void SetUp() override{Test::SetUp();};
     void TearDown() override{Test::TearDown();};
 };
 
 /* TODO
- * Test handleOp
- * Maybe it is smarter to use instantiate some Rpn objects, instead of comparing vectors and stacks...
+ * Test Rpn
+ * Maybe it is smarter to use instantiations of some Rpn objects, instead of comparing vectors and stacks...
  */
-class RpnTest: ::testing::Test{
+class RpnTest:public ::testing::Test{
 protected:
     Rpn rpn;
     void SetUp() override{Test::SetUp();};
     void TearDown() override{Test::TearDown();};
 };
-struct handleState {
-    Op* operation;
-    vector<int> initial_ptvec;
-    vector<int> expected_ptvec;
-    stack<Circuit> initial_stack;
-    stack<Circuit> expected_stack;
-};
-class HandleOpTest: RpnTest, ::testing::WithParamInterface<handleState>{
+
+/*
+ * TODO
+ * Test HandleOp
+ */
+class HandleOpTest: public ::testing::Test{
 protected:
     Op* operation;
     vector<int> ptvec;
     stack<Circuit> s;
+
+    vector<int> expected_ptvec;
+    stack<Circuit> expected_s;
     void SetUp() override
     {
-        operation = GetParam().operation;
-        ptvec = GetParam().initial_ptvec;
-        s = GetParam().initial_stack;
     };
+    void TearDown() override
+    {
+        delete operation;
+    }
 };
 
-TEST_P(HandleOpTest, Int){
-    operation->handleOp(ptvec, s);
-    EXPECT_EQ(ptvec, GetParam().expected_ptvec);
-    EXPECT_EQ(s, GetParam().expected_stack);
+TEST_F(HandleOpTest, IntOnEmptyState){
+    operation = (Op *) new IntOp(1);
+    expected_s.push(single_unary_gate_circuit(Gate::Alias));
+    expected_ptvec.push_back(1);
+
+    operation->handleOp(ptvec,s);
+
+    EXPECT_EQ(ptvec, expected_ptvec);
+    EXPECT_EQ(s,expected_s);
 }
 
-INSTANTIATE_TEST_CASE_P(Default, HandleOpTest,
-        testing::Values(
-                handleState{}
-                )
-        );
+TEST_F(HandleOpTest, IntOnSomeState){
+    operation = (Op *) new IntOp(42);
+    s.push(single_binary_gate_circuit(Gate::Multiply)); //some circuits previously residing on stack...
+    s.push(single_unary_gate_circuit(Gate::Negate));
+    ptvec.push_back(1);
+    ptvec.push_back(3);
+    expected_ptvec = ptvec;
+    expected_ptvec.push_back(42);
+    expected_s = s;
+    expected_s.push(single_unary_gate_circuit(Gate::Alias));
 
+    operation->handleOp(ptvec, s);
+
+    EXPECT_EQ(ptvec, expected_ptvec);
+    EXPECT_EQ(s,expected_s);
+}
+
+//TODO also test exception raised on empty stack.
+
+//TODO test on one alias gate
+TEST_F(HandleOpTest, UnOpOnSimpleState){
+    operation = (Op *) new UnOp(unoptype::Negate);
+
+}
+
+//TODO test on some complicated setting
+
+//TODO test all binop cases
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
