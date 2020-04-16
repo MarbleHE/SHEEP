a67dfae8b45b295930ac59f9cf00d154fbe3b26c
Alexander Viand <alexander.viand@inf.ethz.ch>
2020-04-16 14:00:54 +0200

    update seal-example to work with new seal-BFV context

--- backend/applications/examples/seal-example.cpp
+++ backend/applications/examples/seal-example.cpp
@@ -1,6 +1,6 @@
 #include <memory>
 
-#include "context-seal.hpp"
+#include "context-seal-bfv.hpp"
 
 #include "circuit-repo.hpp"
 
@@ -20,24 +20,24 @@ int main(void) {
   // Circuit C2 = cr.create_circuit(Gate::Add, 3);
   // std::cout << C2;
 
-  ContextSeal<int8_t> ctx;
+  ContextSealBFV<int8_t> ctx;
 
-  // ContextSeal<int8_t>::CircuitEvaluator run_circuit;
+  // ContextSealBFV<int8_t>::CircuitEvaluator run_circuit;
   // run_circuit = ctx.compile(C);
 
-  std::list<ContextSeal<int8_t>::Plaintext> plaintext_inputs = {1, 1, 3, 0};
-  std::list<ContextSeal<int8_t>::Ciphertext> ciphertext_inputs;
+  std::vector<std::vector<int8_t>> plaintext_inputs = {{1}, {1}, {3}, {0}};
+  std::list<ContextSealBFV<int8_t>::Ciphertext> ciphertext_inputs;
 
-  for (ContextSeal<int8_t>::Plaintext pt : plaintext_inputs)
+  for (std::vector<int8_t> pt : plaintext_inputs)
     ciphertext_inputs.push_back(ctx.encrypt(pt));
 
-  std::list<ContextSeal<int8_t>::Ciphertext> ciphertext_outputs;
+  std::list<ContextSealBFV<int8_t>::Ciphertext> ciphertext_outputs;
   using microsecond = std::chrono::duration<double, std::micro>;
   microsecond time = ctx.eval(C, ciphertext_inputs, ciphertext_outputs);
 
-  std::list<ContextSeal<int8_t>::Plaintext> plaintext_outputs;
-  for (ContextSeal<int8_t>::Ciphertext ct : ciphertext_outputs) {
-    ContextSeal<int8_t>::Plaintext pt = ctx.decrypt(ct);
+  std::vector<int8_t> plaintext_outputs;
+  for (ContextSealBFV<int8_t>::Ciphertext ct : ciphertext_outputs) {
+    int8_t pt = ctx.decrypt(ct).at(0);
     plaintext_outputs.push_back(pt);
     std::cout << "output: " << std::to_string(pt) << std::endl;
   }
